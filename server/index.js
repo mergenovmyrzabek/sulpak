6a7a244e64985201396b2a84258b21c7620b764f
Arshidin <arsh.osmanov@gmail.com>
2022-11-16 17:10:09 +0600

    Добавлены маршруты

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const sequelize = require('./db.js')
 const models = require('./models/models.js');
 const cors = require('cors');
+const router = require('./routes/index.js');
 const PORT = 3000;
 
 const app = express();
 app.use(cors);
 app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
     try {
---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+const Router = require('express');
+const router = new Router();
+const deviceRouter = require('./deviceRouter.js');
+const userRouter = require('./userRouter.js');
+const brandRouter = require('./typeRouter.js');
+const typeRouter = require('./typeRouter.js');
+
+router.use('/user', userRouter);
+router.use('/brand', brandRouter);
+router.use('/type', typeRouter);
+router.use('/device', deviceRouter);
+
+module.export = router;
