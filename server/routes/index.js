2bd8255c56b04dd70f6b587ba51053cd1a146a9e
Arshidin <arsh.osmanov@gmail.com>
2022-11-17 13:29:03 +0600

    Исправлены ошибки

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const Router = require('express');
 const router = new Router();
 const deviceRouter = require('./deviceRouter.js');
 const userRouter = require('./userRouter.js');
-const brandRouter = require('./typeRouter.js');
+const brandRouter = require('./brandRouter.js');
 const typeRouter = require('./typeRouter.js');
 
 router.use('/user', userRouter);
