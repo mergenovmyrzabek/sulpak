0ac423d970eefe84876345fa7ade2700d25a09b8
Arshidin <arsh.osmanov@gmail.com>
2022-11-19 16:49:30 +0600

    Создан CRUD для типов

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -3,11 +3,14 @@ const ApiError = require('../error/ApiError.js');
 
 class TypeController {
     async create(request, responce) {
-
+        const {name} = request.body;
+        const type = await Type.create({name});
+        return responce.json(type);
     }
 
     async getAll(request, responce) {
-
+        const types = await Type.findAll();
+        return responce.json(types);
     }
 
 }
