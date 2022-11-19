0fc21e5f7af69b0ded2450f650788efc88e27574
Arshidin <arsh.osmanov@gmail.com>
2022-11-19 13:55:12 +0600

    Подключен middleware

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const sequelize = require('./db.js')
 const models = require('./models/models.js');
 const cors = require('cors');
 const router = require('./routes/index.js');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware.js');
 const PORT = 3000;
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
 app.use('/api', router);
-
 const start = async () => {
     try {
         await sequelize.authenticate();
